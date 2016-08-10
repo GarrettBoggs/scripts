feda333327c7dc34a2ed1d3fefec695a16340944
Ben Furdui and Garrett Boggs <garrett@>
2016-08-10 08:49:02 -0700

    add uppercase function to the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("#blanks form").submit(function() {
-    var person1Input= $("input#person1").val();
+    var person1Input= $("input#person1").val().toUpperCase();
 
-    var person2Input= $("input#person2").val();
+    var person2Input= $("input#person2").val().toUpperCase();
 
     $(".person1").text(person1Input);
 
     $(".person2").text(person2Input);
 
-    
+
     $("#story").show();
 
      event.preventDefault();
